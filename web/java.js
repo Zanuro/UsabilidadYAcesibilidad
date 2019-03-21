52f172dbe19b653e30842b7387e2b262c9bd8dcb
Vlad Comanescu <alu0100968590@ull.edu.es>
2019-03-21 01:18:27 +0000

    web con materialize

---
+++ web/java.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+	$(".dropdown-trigger").dropdown();
+	$('.collapsible').collapsible();
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true
+	  });
+	$('.tabs').tabs();
+
+});
+
+
+
+
+window.alert('otro');
