4baeb5f3bd2d4196496e92a01114f8b154b310f1
Vlad Comanescu <alu0100968590@ull.edu.es>
2019-05-02 02:04:55 +0100

    mas cambios

---
+++ web/func.js
@@ -0,0 +1,142 @@
+/*
+$(document).ready(function() {
+	$(".dropdown-trigger").dropdown();
+	$('.collapsible').collapsible();
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true
+	  });
+	$('.tabs').tabs();
+});
+*/
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.dropdown-trigger');
+	var instances = M.Dropdown.init(elems, options);
+});
+
+
+//window.alert('otro');
+
+function count_element() {
+	var count,count1,count2 = 0;
+	count = document.getElementsByTagName('p').length;
+	count1 = document.getElementsByTagName('a').length;
+	count2 = document.getElementsByTagName('ul').length;
+
+	return [count,count1,count2];
+	windows.alert(count);
+	windows.alert(count1);
+	windows.alert(count2);
+}
+
+var productos = {
+
+		"Productos": [
+			{"Nombre" : "Platano", "Precio" : 1.0},
+			{"Nombre" : "Pan", "Precio" : 1.5},
+			{"Nombre" : "Hamburguesa", "Precio" : 2.3},
+			{"Nombre" : "Agua", "Precio" : 0.8}
+		],
+		calcular_igic : function() {
+			for( var i in productos.Productos){
+				//console.log(productos.Productos[i].Nombre);
+				console.log(`Precio antes de aplicarle IGIC: ${productos.Productos[i].Nombre} --> ${productos.Productos[i].Precio.toPrecision(3)} \n`);
+				productos.Productos[i].Precio += productos.Productos[i].Precio * 0.065
+				console.log(`Precio despues de aplicarle IGIC: ${productos.Productos[i].Nombre} --> ${productos.Productos[i].Precio.toPrecision(3)} \n`);
+				//return(productos.Productos[i].Nombre);
+			}
+		}
+};
+
+
+function devolver_monedas(monedas,cantidad){
+
+	if(cantidad >= monedas[2]["key"]){
+		cantidad -= monedas[2]["key"];
+		monedas[2]["value"] += 1;
+		devolver_monedas(monedas,cantidad);
+	}
+	else if(cantidad < monedas[2]["key"] && cantidad >= monedas[1]["key"]){
+		cantidad -= monedas[1]["key"];
+		monedas[1]["value"] += 1;
+		devolver_monedas(monedas,cantidad);
+	}
+	else if(cantidad < monedas[1]["key"] && cantidad >= monedas[0]["key"]){
+		cantidad -= monedas[0]["key"];
+		monedas[0]["value"] += 1;
+		devolver_monedas(monedas,cantidad);
+	}
+	else if(cantidad == 0){
+		console.log('No se puede devolver ningun cambio');
+	}
+	else if(cantidad < monedas[0]["key"]){
+		console.log('No hay monedas para poder devolver');
+	}
+	return monedas;
+}
+
+
+function cambio_monedas(cantidad){
+
+	var monedas = [{key: 1, value: 0},
+				   {key: 2, value: 0},
+				   {key: 5, value: 0}];
+
+	console.log(`La cantidad es:  ${cantidad}`);
+	monedas = devolver_monedas(monedas,cantidad);
+	console.log('El cambio que se devuelve es: ')
+	for( i in monedas){
+		console.log(`${monedas[i]["value"]} monedas de ${monedas[i]["key"]}\n`);
+	};
+}
+
+
+$("#boton_registro").click(function(){
+
+const peticion_http = new XMLHttpRequest();
+const url = 'https://jsonplaceholder.typicode.com/comments?postId=49';
+
+
+var id;
+peticion_http.open("GET", url);
+
+var nombres = [];
+var emails = [];
+
+peticion_http.onreadystatechange = function () {
+
+	if(peticion_http.readyState == 4 && peticion_http.status == 200){
+		console.log(peticion_http.responseText);
+
+		//alert(peticion_http.responseText);
+
+		var elementos = JSON.parse(peticion_http.responseText);
+
+		for(var i in elementos){
+			nombres.push(elementos[i].name);
+			emails.push(elementos[i].email);
+		}
+
+		for(var i in nombres){
+			$("#nombres").append(`${i} : ${nombres[i]}\n`);
+			$("#email").append(`${i} : ${emails[i]}\n`);
+		}
+		//document.getElementById("").body=elementos.first;
+	}
+	else{
+		console.error('No se ha cargado la pagina');
+	}
+}
+
+peticion_http.send();
+});
+
+var email = document.getElementById("mail");
+
+email.addEventListener("keyup", function (event) {
+  if(email.validity.typeMismatch){
+    email.setCustomValidity("Dirección de correo erronea,por favor introduzca una direccion de correo valida!");
+  }
+  else{
+	  email.setCustomValidity("")
+  }
+});
--- web/public/javascript/firebase.js
+++ web/public/javascript/firebase.js
@@ -1,32 +1,63 @@
 var myDatabase = firebase.database();
 
+var bool_genero=false;
+var bool_title=false,bool_autor=false,bool_agno=false,bool_isbn=false;
 
 document.getElementById("submit").addEventListener('click',function(){
-    var generos=["comedia","aventura","policiaca","historia","ficcion","clasicos"];
-    let bool;
+    
+    var generos=["Comedia","Aventura","Policiaca","Historia","Ficcion","Clasicos"];
     if(document.getElementById("libro").value === ""){
         document.getElementById("libro").style.borderColor="red";
-        document.getElementById("libro")+="Rellena este campo";
+        document.getElementById("alerta-title").innerHTML="Aqui se debe rellenar con el titulo del libro";
+        bool_title=false;
     }
     else{
         document.getElementById("libro").style.borderColor="green";
+        document.getElementById("alerta-title").innerHTML="";
+        bool_title=true;
+
     }
     if(document.getElementById("nombre").value === ""){
         document.getElementById("nombre").style.borderColor="red";
-        document.getElementById("nombre").innerHTML="Rellena este campo";
+        document.getElementById("alerta-autor").innerHTML="Aqui se debe rellenar con el autor del libro";
+        bool_autor=false;
     }
     else{
         document.getElementById("nombre").style.borderColor="green";
+        document.getElementById("alerta-autor").innerHTML="";
+        bool_autor=true;
+
     }
-    if (document.getElementById("genero").validity.patternMismatch){  
-        bool=false;  
+    if (document.getElementById("agnos").value === "" || document.getElementById("agnos").value < "1900" || document.getElementById("agnos").value > "2019"){
+        document.getElementById("agnos").style.borderColor="red";
+        document.getElementById("alerta-agno").innerHTML="No se ha introducido un valor o bien el valor es mayor que 2019 o menor que 1900";
+    }
+    else{
+        document.getElementById("agnos").style.borderColor="green";
+        document.getElementById("alerta-agno").innerHTML= " ";
+
+    }
+    if (!generos.includes(document.getElementById("genero").value)){  
+        bool_genero=false;  
         document.getElementById("genero").style.borderColor="red";
+        document.getElementById("alerta-genero").innerHTML="Los generos disponibles son: Comedia Aventura Policiaca Historia Ficcion Clasicos";
     }  
     else {  
         document.getElementById("genero").style.borderColor="green";
-        bool=true;
+        bool_genero=true;
+        document.getElementById("alerta-genero").innerHTML= " ";
     }
-    if(bool=true){
+    if (document.getElementById("isbn").value.length != 13){
+        document.getElementById("isbn").style.borderColor="red";
+        document.getElementById("alerta-isbn").innerHTML="Debe introducirse un ISBN de exactamente 13 cifras";
+    }
+    else {
+        document.getElementById("isbn").style.borderColor="green";
+        document.getElementById("alerta-isbn").innerHTML="";
+
+    }
+    if(bool_genero==true && bool_title==true && bool_autor==true){
+        alert('El libro se ha añadido con exito');
         var refDB = myDatabase.ref().child('libros');
         var refDC = refDB.child(document.getElementById("libro").value);
         var obj={
@@ -37,11 +68,15 @@ document.getElementById("submit").addEventListener('click',function(){
         };
         refDC.set(obj);
     }
+
 })
 
+document.getElementById("reset").addEventListener('click',function(){
+    $('#reset').trigger("reset")
+})
 document.getElementById("change").addEventListener('click',function(){
     var generos=["comedia","aventura","policiaca","historia","ficcion","clasicos"];
-    let bool;
+    var bool;
     if(document.getElementById("libro").value === ""){
         document.getElementById("libro").style.borderColor="red";
     }
@@ -62,7 +97,7 @@ document.getElementById("change").addEventListener('click',function(){
         document.getElementById("genero").style.borderColor="green";
         bool=true;
     }
-    if(bool=true){
+    if(bool==true){
         var refDB = myDatabase.ref().child('libros');
         var refDC = refDB.child(document.getElementById("libro").value);
         var obj={
